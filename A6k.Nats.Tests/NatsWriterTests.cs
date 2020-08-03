0913d18d406b660dbb0b29ce78eb93facccc3de7
Andreas Ulrik Gehrke (ANDG) <andg@saxobank.com>
2020-08-03 20:03:11 +0200

    Add tests of NatsWriter.WriteJson

--- A6k.Nats.Tests/NatsWriterTests.cs
+++ A6k.Nats.Tests/NatsWriterTests.cs
@@ -25,5 +25,56 @@ namespace A6k.Nats.Tests
             var writtenString = Encoding.UTF8.GetString(buffer.WrittenSpan);
             Assert.Equal(number.ToString(), writtenString);
         }
+
+        [Fact]
+        public void WriteJson_must_serialize_large_object_as_utf8_json_into_buffer()
+        {
+            var objectToSerialize = new SerializableObject { SomeProp = 123, AnotherProp = new string('A', 1024) };
+            var buffer = new ArrayBufferWriter<byte>(initialCapacity: 128);
+            var sut = new NatsWriter(buffer);
+            sut.WriteJson(objectToSerialize);
+            sut.Commit();
+
+            var writtenString = Encoding.UTF8.GetString(buffer.WrittenSpan);
+            var deserializedObject = System.Text.Json.JsonSerializer.Deserialize<SerializableObject>(writtenString);
+            Assert.Equal(objectToSerialize.SomeProp, deserializedObject.SomeProp);
+            Assert.Equal(objectToSerialize.AnotherProp, deserializedObject.AnotherProp);
+        }
+
+        [Fact]
+        public void WriteJson_followed_by_other_writes_must_serialize_object_as_utf8_json_into_buffer()
+        {
+            var objectToSerialize = new SerializableObject { SomeProp = 123, AnotherProp = "foobar" };
+            var buffer = new ArrayBufferWriter<byte>();
+            var sut = new NatsWriter(buffer);
+            sut.WriteJson(objectToSerialize);
+            sut.Write(new byte[] { 65, 66, 67, 68 }); // ABCD as ASCII
+            sut.Commit();
+
+            var writtenString = Encoding.UTF8.GetString(buffer.WrittenSpan);
+            Assert.EndsWith("ABCD", writtenString);
+        }
+
+        [Fact]
+        public void WriteJson_preceded_by_other_writes_must_serialize_object_as_utf8_json_into_buffer()
+        {
+            var objectToSerialize = new SerializableObject { SomeProp = 123, AnotherProp = "foobar" };
+            var buffer = new ArrayBufferWriter<byte>();
+            var sut = new NatsWriter(buffer);
+            sut.Write(new byte[] { 65, 66, 67, 68 }); // ABCD as ASCII
+            sut.WriteJson(objectToSerialize);
+            sut.Write(new byte[] { 48, 49, 50, 51 }); // 0123 as ASCII
+            sut.Commit();
+
+            var writtenString = Encoding.UTF8.GetString(buffer.WrittenSpan);
+            Assert.StartsWith("ABCD", writtenString);
+            Assert.EndsWith("0123", writtenString);
+        }
+
+        public class SerializableObject
+        {
+            public int SomeProp { get; set; }
+            public string AnotherProp { get; set; }
+        }
     }
 }
